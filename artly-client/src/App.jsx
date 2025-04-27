e21b36a3603733c93a98c4ba95c6d7cd714b5571
P1su <pissssssu@naver.com>
2025-04-27 13:19:57 +0900

    'first-commit'

---
+++ artly-client/src/App.jsx
@@ -0,0 +1,7 @@
+import router from './router/Router';
+import { RouterProvider } from 'react-router-dom';
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+export default App;
---
+++ artly-client/src/pages/Main/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <div>
+      <h1>아뜰리</h1>
+      메인페이지입니다.
+    </div>
+  );
+}
