7dea10af11318bab0a50efc6536ee2bdb45ac012
P1su <pissssssu@naver.com>
2025-04-27 22:53:57 +0900

    feat: filterList 배열값을 FilterTab의 props로 전달

--- client/src/components/FilterTab/FilterTab.jsx
+++ client/src/components/FilterTab/FilterTab.jsx
@@ -1,11 +1,10 @@
 import styles from './FilterTab.module.css';
 
-export default function FilterTab() {
-  const tmpList = ['최신순', '인기순', '날짜순'];
+export default function FilterTab({ filterList }) {
   return (
     <div className={styles.layout}>
       <ul className={styles.filterList}>
-        {tmpList.map((item) => (
+        {filterList.map((item) => (
           <li className={styles.filterItem}>{item}</li>
         ))}
       </ul>
