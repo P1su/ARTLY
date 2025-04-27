94cc8a09535c3b342330192cb2f1a7d9c7e85306
P1su <pissssssu@naver.com>
2025-04-28 00:51:13 +0900

    feat: Dropdown 컴포넌트 제작

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,5 +1,6 @@
 import styles from './Main.module.css';
 import FilterTab from '../../components/FilterTab/FilterTab';
+import Dropdown from '../../components/Dropdown/Dropdown';
 
 export default function Main() {
   const filterList = ['최신순', '인기순', '날짜순', '조회순'];
@@ -9,6 +10,7 @@ export default function Main() {
       <h1>아뜰리</h1>
       메인페이지입니다.
       <FilterTab filterList={filterList} />
+      <Dropdown />
     </div>
   );
 }
