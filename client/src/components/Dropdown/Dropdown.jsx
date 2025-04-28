7a085c4a4014f51a45ded671862b43308f997527
P1su <pissssssu@naver.com>
2025-04-28 14:18:57 +0900

    feat: 마우스 Over 와 Out 이벤트로 드롭다운 렌더링되도록 구현

--- client/src/components/Dropdown/Dropdown.jsx
+++ client/src/components/Dropdown/Dropdown.jsx
@@ -3,12 +3,10 @@ import styles from './Dropdown.module.css';
 export default function Dropdown() {
   const tmp = ['최신순', '오래된순', 'Option3', 'Option4'];
   return (
-    <div className={styles.layout}>
-      <ul className={styles.dropdownList}>
-        {tmp.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <ul className={styles.dropdownList}>
+      {tmp.map((item) => (
+        <li>{item}</li>
+      ))}
+    </ul>
   );
 }
--- client/src/components/FilterTab/FilterTab.jsx
+++ client/src/components/FilterTab/FilterTab.jsx
@@ -1,11 +1,26 @@
 import styles from './FilterTab.module.css';
+import { useState } from 'react';
+import Dropdown from '../Dropdown/Dropdown';
 
 export default function FilterTab({ filterList }) {
+  const [displayNum, setDisplayNum] = useState(-1);
+
   return (
     <div className={styles.layout}>
       <ul className={styles.filterList}>
-        {filterList.map((item) => (
-          <li className={styles.filterItem}>{item}</li>
+        {filterList.map((item, idx) => (
+          <div
+            key={item}
+            onMouseOver={() => {
+              setDisplayNum(idx);
+            }}
+            onMouseOut={() => {
+              setDisplayNum(-1);
+            }}
+          >
+            <li>{item}</li>
+            {displayNum === idx && <Dropdown />}
+          </div>
         ))}
       </ul>
     </div>
