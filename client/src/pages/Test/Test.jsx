e132df5fd5d6a6ae512b563a8759381747522375
P1su <pissssssu@naver.com>
2025-04-28 14:44:35 +0900

    feat: Test 페이지 생성

---
+++ client/src/pages/Test/Test.jsx
@@ -0,0 +1,10 @@
+import styles from './Test.module.css';
+
+export default function Test() {
+  return (
+    <div className={styles.layout}>
+      <h1>아뜰리</h1>
+      테스트페이지입니다.
+    </div>
+  );
+}
