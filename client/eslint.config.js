2257c566822382f25c17b256b04591664044f23f
P1su <pissssssu@naver.com>
2025-04-29 13:33:47 +0900

    add: eslint-plugin-react 설치

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,11 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  {
+    ignores: ['dist'], // 검사 제외 폴더
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js },
+    extends: ['js/recommended', 'prettier'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: { globals: globals.browser },
+  },
   pluginReact.configs.flat.recommended,
 ]);
