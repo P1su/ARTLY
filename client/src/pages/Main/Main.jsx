7bc1e4efbf82b8cff750d4c289b52f0fe21b709a
P1su <pissssssu@naver.com>
2025-05-01 22:26:00 +0900

    style: 필요없는 코드 정리

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import SearchBar from '../../components/common/SearchBar/SearchBar';
-import Footer from '../../components/common/Footer/Footer';
 import styles from './Main.module.css';
 
 export default function Main() {
   return (
     <div className={styles.mainLayout}>
-      <main className={styles.mainContent}>
-        <SearchBar />
-      </main>
+      <SearchBar />
     </div>
   );
 }
