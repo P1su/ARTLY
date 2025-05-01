a4bafe3fddf6419c9f25b47af099dfc4fc76554c
P1su <pissssssu@naver.com>
2025-05-01 22:18:49 +0900

    feat: Layout 컴포넌트 제작 후 중첩라우팅 적용

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,28 +1,14 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
-import NavBar from '../../components/NavBar/NavBar';
-import SearchBar from "../../components/common/SearchBar/SearchBar";
+import SearchBar from '../../components/common/SearchBar/SearchBar';
 import Footer from '../../components/common/Footer/Footer';
-import ChatbotWidget from '../../components/common/ChatbotWidget/ChatbotWidget';
 import styles from './Main.module.css';
 
 export default function Main() {
   return (
-    <>
-      <Header />
-      <div className={styles.mainLayout}>
-        <NavBar />
-        <main className={styles.mainContent}>
-          <h1>아뜰리</h1>
-          <SearchBar />
-          <br />
-          메인페이지입니다.
-          <br />
-          <br />
-          <Footer />
-          <ChatbotWidget />
-        </main>
-      </div>
-    </>
+    <div className={styles.mainLayout}>
+      <main className={styles.mainContent}>
+        <SearchBar />
+      </main>
+    </div>
   );
 }
