0bc4a046d56d42ce9faf4f816baf69ecd345fd38
송 필 수 <104068583+P1su@users.noreply.github.com>
2025-05-02 12:20:03 +0900

    Develop에서 main merge (#8)
    * [CHORE] ESLint 및 Prettier 설정 (#1)
    
    * add: eslint, prettier 설치
    
    * add: eslint 와 prettier 충돌 방지 라이브러리 설치
    
    * add: eslint-plugin-react 설치
    
    * feat: eslint 설정
    
    * feat: prettier 설정
    
    * [FEAT] 필터와 드롭다운 구현 (#2)
    
    * feat: FilterTab 레이아웃 설정
    
    * style: 공통된 스타일 수정
    
    * feat: filterList 배열값을 FilterTab의 props로 전달
    
    * feat: Dropdown 컴포넌트 제작
    
    * feat: 마우스 Over 와 Out 이벤트로 드롭다운 렌더링되도록 구현
    
    * feat: Test 페이지 생성
    
    * feat: FilterTab을 페이지에서 관리하도록 수정
    
    * feat: SearchList 컴포넌트 생성
    
    * style: Test 페이지 필요없는 css 삭제
    
    * feat: vscode 폴더 삭제
    
    * chore: pr 템플릿 작성
    
    * fix: .github 디렉토리 위치 변경
    
    * style: lint 및 prettier 설정 수정
    
    * style: FilterTab 폴더 및 파일 삭제
    
    * feat: filterist mockdata 형식을 객체로 변경 및 간단한 css 와 네이밍 수정
    
    * style: 픽셀 단위 수정
    
    * feat: Chatbot, SearchBar, Footer 컴포넌트 추가
    
    * fix: 깃 오류 수정 및 컴포넌트 전체 스타일 수정
    
    * [FEAT] Header와 NavBar 구현 
    
    * feat: Header, NavBar 컴포넌트 추가
    
    * feat: Header, NavBar 컴포넌트 추가
    
    * feat: App.jsx에 기능 추가
    
    * chore: pr 템플릿 작성
    
    * fix: .github 디렉토리 위치 변경
    
    * refactor: App.jsx 내부 컴포넌트 구조 정리
    
    * chore: 패키지 업데이트
    
    * style: Header, NavBar 스타일 분리
    
    * refactor: 컴포넌트를 tsx에서 jsx로 변경
    
    * fix: 요구사항 수정
    
    ---------
    
    Co-authored-by: hsji <bifur@03kau.kr>
    Co-authored-by: P1su <pissssssu@naver.com>
    
    * [FEAT] 레이아웃 설정 (#6)
    
    * feat: Layout 컴포넌트 제작 후 중첩라우팅 적용
    
    * style: 필요없는 코드 정리
    
    * style: Layout position 설정
    
    * feat: LayoutWithSearchbar 구현
    
    * feat: 서치바 여부에 따른 라우트 구분
    
    * style: 챗봇 스타일 수정
    
    * style: 챗봇 bottom 위치 수정
    
    ---------
    
    Co-authored-by: 홍유진 <dbwlsyyy@naver.com>
    Co-authored-by: dbwlsyyy <148929571+dbwlsyyy@users.noreply.github.com>
    Co-authored-by: hsji <127402491+kavewu57@users.noreply.github.com>
    Co-authored-by: hsji <bifur@03kau.kr>

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,8 +1,6 @@
+import React from 'react';
+import styles from './Main.module.css';
+
 export default function Main() {
-  return (
-    <div>
-      <h1>아뜰리</h1>
-      메인페이지입니다.
-    </div>
-  );
+  return <div className={styles.mainLayout} />;
 }
--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,10 +1,49 @@
+import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../layouts/Layout';
+import LayoutWithSearchbar from '../layouts/LayoutWithSearchbar';
 import Main from '../pages/Main/Main';
+import Test from '../pages/Test/Test';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Main />,
+    element: <Layout />,
+    children: [
+      {
+        element: <LayoutWithSearchbar />,
+        children: [
+          {
+            path: '/',
+            element: <Main />,
+          },
+          {
+            path: '/exhibitions',
+            element: <div>전시회 페이지</div>,
+          },
+          {
+            path: '/galleries',
+            element: <div>갤러리 페이지</div>,
+          },
+          {
+            path: '/artists',
+            element: <div>작가 페이지</div>,
+          },
+          {
+            path: '/notices',
+            element: <div>공고 페이지</div>,
+          },
+        ],
+      },
+      {
+        path: '/test',
+        element: <Test />,
+      },
+      {
+        path: '/nearby-galleries',
+        element: <div>주변 갤러리 페이지</div>,
+      },
+    ],
   },
 ]);
 
