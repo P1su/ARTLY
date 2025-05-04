5972074d8088fc0f0ade7a212705eed8660430b3
홍유진 <dbwlsyyy@naver.com>
2025-05-04 20:17:21 +0900

    style: coponents 폴더 구조 수정  및 챗봇토글  위치 조정

--- client/src/components/ChatbotWidget/ChatbotWidget.jsx
+++ client/src/components/ChatbotWidget/ChatbotWidget.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import styles from './ChatbotWidget.module.css';
-import chatbotIcon from '../../../assets/images/chatbot.png';
-import chatbotProfile from '../../../assets/images/chatbotProfile.png';
+import chatbotIcon from '../../assets/images/chatbot.png';
+import chatbotProfile from '../../assets/images/chatbotProfile.png';
 
 export default function ChatbotWidget() {
   const [isOpen, setIsOpen] = useState(false);
--- client/src/layouts/LayoutWithSearchbar.jsx
+++ client/src/layouts/LayoutWithSearchbar.jsx
@@ -1,6 +1,6 @@
 import useMobile from '../hooks/useMobile';
 import { Outlet } from 'react-router-dom';
-import SearchBar from '../components/common/SearchBar/SearchBar';
+import SearchBar from '../components/SearchBar/SearchBar';
 
 export default function LayoutWithSearchbar() {
   useMobile();
