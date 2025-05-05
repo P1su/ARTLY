516490d055360742313965344c94cf422aa4dc0c
hsji <bifur@03kau.kr>
2025-05-05 14:37:41 +0900

    fix: develop 병합을 위한 임시 저장

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,68 +1,24 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; 
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Menu from '../Menu/Menu';
 
 export default function Header() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [nickname, setNickname] = useState('');
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-    setNickname('아뜰리');
-    setIsMenuOpen(false);
-  };
-
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setNickname('');
-    setIsMenuOpen(false);
-  };
-
-  const handleToggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
+  const handleOpen = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
     <header className={styles.headerLayout}>
       <h1 className={styles.logoBox}>
-        <Link to="/" className={styles.logoLink}>Artly</Link>
+        <Link to='/' className={styles.logoLink}>
+          Artly
+        </Link>
       </h1>
-
-      <div className={styles.userContainer}>
-        {isLoggedIn ? (
-          <>
-            <div className={styles.nicknameWrapper} onClick={handleToggleMenu}>
-              <span>
-                <span className={styles.nicknameText}>{nickname}</span>
-                <span className={styles.nicknameSuffix}> 님</span>
-              </span>
-              <span className={styles.dropdownSpan}>▼</span>
-            </div>
-
-            <button type="button" className={styles.pageButton}>
-              마이페이지
-            </button>
-
-            {isMenuOpen && (
-              <div className={styles.dropdownBox}>
-                <button type="button" className={styles.logoutButton} onClick={handleLogout}>
-                  로그아웃
-                </button>
-              </div>
-            )}
-          </>
-        ) : (
-          <>
-            <button type="button" className={styles.authButton} onClick={handleLogin}>
-              로그인
-            </button>
-            <button type="button" className={styles.authButton}>
-              회원가입
-            </button>
-          </>
-        )}
-      </div>
+      <button onClick={handleOpen}>{isOpen ? '닫기' : '메뉴'}</button>
+      {isOpen && <Menu onOpen={handleOpen} />}
     </header>
   );
 }
---
+++ client/src/utils/menu.js
@@ -0,0 +1,32 @@
+export const menuList = [
+    {
+      name: 'mypage',
+      label: '마이페이지',
+      path: '/mypage',
+    },
+    {
+      name: 'exhibition',
+      label: '전시회',
+      path: '/exhibitions',
+    },
+    {
+      name: 'gallery',
+      label: '갤러리',
+      path: '/galleries',
+    },
+    {
+      name: 'artist',
+      label: '작가',
+      path: '/artists',
+    },
+    {
+      name: 'gallary-nearby',
+      label: '주변 갤러리',
+      path: '/gallery-nearby',
+    },
+    {
+      name: 'notice',
+      label: '공고',
+      path: '/notices',
+    },
+  ];
