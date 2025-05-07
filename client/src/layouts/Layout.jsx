540128c6582548981dc85c6a5e7114b5383fccf7
P1su <pissssssu@naver.com>
2025-05-07 18:00:06 +0900

    feat: ScrollTotop 제작

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -2,12 +2,14 @@ import useMobile from '../hooks/useMobile';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import ChatbotWidget from '../components/common/ChatbotWidget/ChatbotWidget';
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
 
 export default function Layout() {
   useMobile();
 
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <ChatbotWidget />
--- client/src/layouts/LayoutWithSearchbar.jsx
+++ client/src/layouts/LayoutWithSearchbar.jsx
@@ -1,12 +1,14 @@
 import useMobile from '../hooks/useMobile';
 import { Outlet } from 'react-router-dom';
 import SearchBar from '../components/common/SearchBar/SearchBar';
+import ScrollToTop from '../components/ScrollToTop/ScrollToTop';
 
 export default function LayoutWithSearchbar() {
   useMobile();
 
   return (
     <>
+      <ScrollToTop />
       <SearchBar />
       <Outlet />
     </>
