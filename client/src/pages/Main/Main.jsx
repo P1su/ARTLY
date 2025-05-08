381d2febf9945d718bf1616cffe0a18d30f57409
hsji <bifur@03kau.kr>
2025-05-08 10:35:20 +0900

    fix: 임시 커밋 저장

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import styles from './Main.module.css';
+import MainCarousel from './components/MainCarousel/MainCarousel';
+import ExhibitionCarousel from './components/ExhibitionCarousel/ExhibitionCarousel';
+import {
+  mainCarousel,
+  weeklyCarousel,
+  interestCarousel
+} from './data/Carousel'; 
+
 
 export default function Main() {
-  return <div className={styles.mainLayout} />;
+  return (
+    <div className={styles.mainLayout}>
+      <MainCarousel items={mainCarousel} />
+      <ExhibitionCarousel title="금주의 전시" items={weeklyCarousel} />
+      <ExhibitionCarousel title="관심 있을 전시" items={interestCarousel} />
+    </div>
+  );
 }
