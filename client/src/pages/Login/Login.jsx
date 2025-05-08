7b60dad59767c09352d4ca2ab5b6642bfcb92d98
hsji <bifur@03kau.kr>
2025-05-08 10:14:45 +0900

    refactor: 로그인 및 회원가입 수정

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import Header from '../../components/Header/Header';
 import Logo from '../../components/Logo/Logo';
 import InputText from '../../components/InputText/InputText';
 import BtnPrimary from '../../components/BtnPrimary/BtnPrimary';
 import Footer from '../../components/common/Footer/Footer';
-import ChatbotWidget from '../../components/common/ChatbotWidget/ChatbotWidget';
 import styles from './Login.module.css';
 
 export default function Login() {
   return (
-    <div className={styles.loginLayout}>
-      <div className={styles.contentWrapper}>
+    <div className={styles.layout}>
+      <div className={styles.contentContainer}>
         <Logo />
         <form className={styles.form}>
           <InputText placeholder="아이디" />
@@ -18,7 +16,6 @@ export default function Login() {
           <BtnPrimary label="Login" />
         </form>
       </div>
-
       <Footer />
     </div>
   );
