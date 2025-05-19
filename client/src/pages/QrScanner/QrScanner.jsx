94912747824a66af915a18075ff3dbe6d83c1340
홍유진 <dbwlsyyy@naver.com>
2025-05-19 16:40:56 +0900

    add: QR 스캐너 라이브러리 설치

--- client/src/pages/QrScanner/QrScanner.jsx
+++ client/src/pages/QrScanner/QrScanner.jsx
@@ -4,9 +4,9 @@ import { useNavigate } from 'react-router-dom';
 export default function QrScanner() {
   const navigate = useNavigate();
 
-  const handleFakeScan = () => {
-    alert('QR 인식 성공! 작품 정보를 불러옵니다.');
-    //navigate('');
+  const handleScan = () => {
+    alert('QR 인식 성공');
+    navigate('art/:artId');
   };
 
   return (
@@ -14,11 +14,11 @@ export default function QrScanner() {
       <h2 className={styles.title}>작품 QR 인식</h2>
       <div className={styles.scannerBox}>
         <div className={styles.cameraArea}>
-          <p className={styles.scanGuide}>QR 코드를 카메라 영역에 맞춰주세요</p>
+          <p className={styles.scanGuide}>QR 코드를 점선 영역에 맞춰주세요</p>
           <div className={styles.scanFrame} />
         </div>
-        <button className={styles.scanButton} onClick={handleFakeScan}>
-          QR 스캐너
+        <button className={styles.scanButton} onClick={handleScan}>
+          QR 인식 성공
         </button>
       </div>
     </div>
