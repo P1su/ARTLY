41edebf244271be5b84394d9a1abda1c9b671ec5
P1su <pissssssu@naver.com>
2025-05-20 23:54:53 +0900

    feat: 검색 api 연결 및 util 함수 분리

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,12 +1,34 @@
 import styles from './Search.module.css';
 import { useLocation } from 'react-router-dom';
-import { mockResults } from './mock/mockSearchResult.js';
+import { useEffect, useState } from 'react';
+import { instance } from '../../apis/instance.js';
+import { filterResults } from './utils/filterResults.js';
 import SearchResultSection from './components/SearchResultSection/SearchResultSection';
 
 export default function Search() {
   const location = useLocation();
   const params = new URLSearchParams(location.search);
   const queryString = params.get('query');
+  const [results, setResults] = useState([]);
+
+  const getSearchResult = async () => {
+    try {
+      const response = await instance.get('/api/search', {
+        params: {
+          search: queryString,
+        },
+      });
+
+      setResults(response.data);
+      console.log(response.data);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  useEffect(() => {
+    getSearchResult();
+  }, [queryString]);
 
   return (
     <div className={styles.layout}>
@@ -15,10 +37,22 @@ export default function Search() {
         검색 결과
       </h1>
       <section className={styles.resultSection}>
-        <SearchResultSection category='exhibition' results={mockResults} />
-        <SearchResultSection category='gallery' results={mockResults} />
-        <SearchResultSection category='artist' results={mockResults} />
-        <SearchResultSection category='notice' results={mockResults} />
+        <SearchResultSection
+          category='exhibition'
+          results={filterResults(results.exhibitions, 'exhibition')}
+        />
+        <SearchResultSection
+          category='gallery'
+          results={filterResults(results.galleries, 'gallery')}
+        />
+        <SearchResultSection
+          category='artist'
+          results={filterResults(results.artists, 'artist')}
+        />
+        <SearchResultSection
+          category='notice'
+          results={filterResults(results.announcements, 'notice')}
+        />
       </section>
     </div>
   );
--- client/src/pages/Search/components/SearchResultSection/SearchResultSection.jsx
+++ client/src/pages/Search/components/SearchResultSection/SearchResultSection.jsx
@@ -1,48 +1,20 @@
 import styles from './SearchResultSection.module.css';
+import { filterCategory } from '../../utils/filterCategory.js';
 import SearchResultItem from '../SearchResultItem/SearchResultItem';
 
 export default function SearchResultSection({ category, results }) {
-  const transCategory = (category) => {
-    switch (category) {
-      case 'exhibition':
-        return {
-          label: '전시회',
-          link: 'exhibitions',
-        };
-      case 'gallery':
-        return {
-          label: '갤러리',
-          link: 'galleries',
-        };
-      case 'artist':
-        return {
-          label: '작가',
-          link: 'artists',
-        };
-      case 'notice':
-        return {
-          label: '공고',
-          link: 'notices',
-        };
-      default:
-        break;
-    }
-  };
-
-  const filterdList = results.filter((item) => item.type === category);
-
   return (
     <section className={styles.layout}>
-      <h2 className={styles.categoryTitle}>{transCategory(category).label}</h2>
+      <h2 className={styles.categoryTitle}>{filterCategory(category).label}</h2>
       <hr />
       <ul className={styles.resultList}>
-        {filterdList.length === 0 ? (
-          <span>검색 결과가 없습니다</span>
+        {!results || results.length === 0 ? (
+          <div className={styles.emptyBox}>검색 결과가 없습니다</div>
         ) : (
-          filterdList.map(({ id, thumbnail, title }) => (
+          results.map(({ id, thumbnail, title }) => (
             <SearchResultItem
               key={id}
-              link={`/${transCategory(category).link}/${id}`}
+              link={`/${filterCategory(category).link}/${id}`}
               thumbnail={thumbnail}
               title={title}
             />
