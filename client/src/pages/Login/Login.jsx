1ea5dad897a69f102f3785cbb95f2f3d59cc9581
P1su <pissssssu@naver.com>
2025-05-22 02:34:55 +0900

    feat: 로그인 페이지 퍼블리싱

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,11 @@
 import styles from './Login.module.css';
 import { instance } from '../../apis/instance.js';
 import useInput from '../../hooks/useInput';
+import InputContainer from '../../components/Input/InputConatiner/InputContainer';
 import InputText from '../../components/Input/InputText/InputText';
 import BtnPrimary from '../../components/BtnPrimary/BtnPrimary';
+import SocialLoginSection from './components/SocialLoginSection/SocialLoginSection';
+import SupportSection from './components/SupportSection/SupportSection';
 
 export default function Login() {
   const { data: loginDatas, handleChange } = useInput({
@@ -24,20 +27,26 @@ export default function Login() {
       <p className={styles.subParagraph}>아뜰리 계정으로 로그인하세요</p>
       <div className={styles.contentContainer}>
         <form className={styles.form} action={postLogin}>
-          <InputText
-            placeholder='아이디'
-            name='login_id'
-            onChange={handleChange}
-            value={loginDatas.login_id}
-          />
-          <InputText
-            placeholder='비밀번호'
-            name='login_pwd'
-            onChange={handleChange}
-            value={loginDatas.login_pwd}
-          />
-          <BtnPrimary label='Login' />
+          <InputContainer title='아이디'>
+            <InputText
+              placeholder='아이디를 입력해주세요'
+              name='login_id'
+              onChange={handleChange}
+              value={loginDatas.login_id}
+            />
+          </InputContainer>
+          <InputContainer title='비밀번호'>
+            <InputText
+              placeholder='비밀번호'
+              name='login_pwd'
+              onChange={handleChange}
+              value={loginDatas.login_pwd}
+            />
+          </InputContainer>
+          <BtnPrimary label='로그인' />
         </form>
+        <SocialLoginSection />
+        <SupportSection />
       </div>
     </div>
   );
