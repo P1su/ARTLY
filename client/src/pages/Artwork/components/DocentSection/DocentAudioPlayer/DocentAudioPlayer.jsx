bdc7990209bd9802fa2aadebff1c747798b13ad3
hsji <bifur@03kau.kr>
2025-05-21 14:37:54 +0900

    code review: 오디오 적용

--- client/src/pages/Artwork/components/DocentSection/DocentAudioPlayer/DocentAudioPlayer.jsx
+++ client/src/pages/Artwork/components/DocentSection/DocentAudioPlayer/DocentAudioPlayer.jsx
@@ -1,10 +1,11 @@
 import styles from './DocentAudioPlayer.module.css';
+import mockAudio from '../../../mock/mockAudio.mp3';
 
 export default function DocentAudioPlayer() {
   return (
     <div className={styles.audioPlayerWrapper}>
       <audio controls>
-        <source src="/audio/sample.mp3" type="audio/mp3" />
+        <source src={mockAudio} type="audio/mp3" />
         Your browser does not support the audio element.
       </audio>
     </div>
