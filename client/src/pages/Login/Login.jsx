75ef37180678fb9b99962b2bfb21f7d62006cb9a
P1su <pissssssu@naver.com>
2025-05-21 20:43:41 +0900

    feat: 로그인 API 연결

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
-import Logo from '../../components/Logo/Logo';
+import styles from './Login.module.css';
+import { instance } from '../../apis/instance.js';
+import { useState } from 'react';
 import InputText from '../../components/InputText/InputText';
 import BtnPrimary from '../../components/BtnPrimary/BtnPrimary';
-import Footer from '../../components/Footer/Footer';
-import styles from './Login.module.css';
 
 export default function Login() {
+  const [loginDatas, setLoginDatas] = useState({
+    login_id: '',
+    login_pwd: '',
+  });
+
+  const handleChange = (e) => {
+    setLoginDatas((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const postLogin = async () => {
+    try {
+      await instance.post('/api/auth/login', loginDatas);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
   return (
     <div className={styles.layout}>
+      <h1 className={styles.loginTitle}>로그인</h1>
+      <p className={styles.subParagraph}>아뜰리 계정으로 로그인하세요</p>
       <div className={styles.contentContainer}>
-        <Logo />
-        <form className={styles.form}>
-          <InputText placeholder='아이디' />
-          <InputText placeholder='비밀번호' />
+        <form className={styles.form} action={postLogin}>
+          <InputText
+            placeholder='아이디'
+            name='login_id'
+            onChange={handleChange}
+            value={loginDatas.login_id}
+          />
+          <InputText
+            placeholder='비밀번호'
+            name='login_pwd'
+            onChange={handleChange}
+            value={loginDatas.login_pwd}
+          />
           <BtnPrimary label='Login' />
         </form>
       </div>
