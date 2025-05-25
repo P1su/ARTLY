20b7afc3317760743f9e23d85dfac0bb8724e680
P1su <pissssssu@naver.com>
2025-05-25 15:20:28 +0900

    feat: 지도 불러오기

--- client/src/pages/Nearby/Nearby.jsx
+++ client/src/pages/Nearby/Nearby.jsx
@@ -1,10 +1,20 @@
 import styles from './Nearby.module.css';
+import { useEffect } from 'react';
 
 export default function Nearby() {
+  useEffect(() => {
+    if (!window.naver) {
+      return;
+    }
+    const map = new naver.maps.Map('map', {
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
+      zoom: 10,
+    });
+  });
   return (
     <div className={styles.layout}>
       <p>주변 갤러리 찾기 페이지,,,</p>
-      <div />
+      <div id='map' className={styles.galleryWrapper} />
     </div>
   );
 }
