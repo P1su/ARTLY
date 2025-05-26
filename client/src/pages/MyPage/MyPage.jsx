d45bde21746fd2ca431b7ac4d9eb44e6edb5a3c0
홍유진 <dbwlsyyy@naver.com>
2025-05-27 06:47:17 +0900

    style: 불필요한 폴더 구조 단순화

--- client/src/pages/MyPage/MyPage.jsx
+++ client/src/pages/MyPage/MyPage.jsx
@@ -3,14 +3,30 @@ import styles from './Mypage.module.css';
 import ProfileCard from './components/ProfileCard/ProfileCard';
 import TabNav from './components/TabNav/TabNav';
 import TabContent from './components/TabContent/TabContent';
+import TabMyArtly from './components/TabContent/TabMyArtly/TabMyArtly';
+import TabLike from './components/TabContent/TabLike/TabLike';
+import TabPurchased from './components/TabContent/TabPurchased/TabPurchased';
 
 export default function MyPage() {
   const [selectedTab, setSelectedTab] = useState('My Artly');
+
+  const renderTabContent = () => {
+    switch (selectedTab) {
+      case 'My Artly':
+        return <TabMyArtly />;
+      case 'Like':
+        return <TabLike />;
+      case '구매 내역':
+        return <TabPurchased />;
+      default:
+        return null;
+    }
+  };
   return (
     <div className={styles.layout}>
       <ProfileCard />
       <TabNav selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
-      <TabContent tab={selectedTab} />
+      {renderTabContent()}
     </div>
   );
 }
