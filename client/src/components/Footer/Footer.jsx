c025e6d50f6e2547aee109221f6fbf8f239bcae8
P1su <pissssssu@naver.com>
2025-05-28 17:30:44 +0900

    feat: Footer 수정

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,28 +1,21 @@
 import styles from './Footer.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <footer className={styles.footerLayout}>
-      <div className={styles.footerContentBox}>
-        <h2>ARTLY</h2>
-        <p>㈜오드레몬오리진 | 사업자 번호: 12345678</p>
-        <p>대표: 홍길동 | 기술책임자: 홍길동</p>
-        <p>경기도 고양시 덕양구 한국항공대학교</p>
-        <p>문의: email@email.com</p>
+      <div className={styles.linkContainer}>
+        <Link className={styles.link}>공지사항&FAQ</Link>
+        <span className={styles.divideSpan}>|</span>
+        <Link className={styles.link}>약관 및 정책</Link>
       </div>
-      <div className={styles.footerLinksContainer}>
-        <div className={styles.footerLinksBox}>
-          <a href="#">서비스 이용 약관</a>
-          <a href="#">개인정보처리방침</a>
-          <a href="#">취소 및 환불 약관</a>
-          <a href="#">전자금융거래 이용약관</a>
-        </div>
-        {/* 추후 이미지로 변경 */}
-        <div className={styles.footerAppsBox}>
-          <a href="#">Google Play에서 다운로드</a>
-          <a href="#">App Store에서 다운로드</a>
-          <a href="#">Instagram</a>
-        </div>
+      <div className={styles.infoContainer}>
+        <p>Copyright © 2024 Artly Corp. All Rights Reserved.</p>
+        <p>상호명 : Artly | 사업자등록번호 : 000-00-00000</p>
+        <p>
+          대외총괄/제안 : info@artly.co.kr | 전시지원/관리 : contact@artly.co.kr
+        </p>
+        <p>홍보/기획 : biz@artly.co.kr</p>
       </div>
     </footer>
   );
