84a66047b5b150d47cb009b36398abf42eda2c81
홍유진 <dbwlsyyy@naver.com>
2025-05-29 12:40:28 +0900

    fix: 삭제된 라이브러리로 인한 오류 발생하여 해당 모듈 제거

--- client/src/pages/QrScanner/QrScanner.jsx
+++ client/src/pages/QrScanner/QrScanner.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Scanner } from '@yudiel/react-qr-scanner';
 import styles from './QrScanner.module.css';
 
 export default function QrScanner() {
