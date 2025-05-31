171d083d88f3ede4d9becf4f768c267f494c3cbb
P1su <pissssssu@naver.com>
2025-05-31 18:09:25 +0900

    feat: 작가 상세 페이지 스타일 수정

--- client/src/pages/ArtistDetail/ArtistDetail.jsx
+++ client/src/pages/ArtistDetail/ArtistDetail.jsx
@@ -1,13 +1,12 @@
 import styles from './ArtistDetail.module.css';
 import { mockArtistDetail } from './mock/mockArtistDetail.js';
 import BtnFavorite from '../ExhibitionDetail/components/BtnFavorite/BtnFavorite'; //추후 확장 및 수정 예정
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { instance } from '../../apis/instance.js';
 import ArtistActivity from './components/ArtistActivity/ArtistActivity';
 
 export default function ArtistDetail() {
-  const { educations, careers } = mockArtistDetail;
   const { artistId } = useParams();
   const [artistData, setArtistData] = useState([]);
 
@@ -33,25 +32,18 @@ export default function ArtistDetail() {
           src={artistData.imageUrl}
           alt='작가 대표 이미지'
         />
-        <span className={styles.titleSpan}>{artistData.name}</span>
-        <span className={styles.subSpan}>{artistData.nation}</span>
-        <span className={styles.subSpan}>{artistData.field}</span>
-        <BtnFavorite />
+        <h3 className={styles.artistTitle}>{artistData.name}</h3>
+        <span className={styles.subSpan}>
+          {artistData.nation} | {artistData.field}
+        </span>
       </section>
-      <p className={styles.descriptionParagraph}>{artistData.description}</p>
-      <section className={styles.descriptionSection}>
-        <span className={styles.titleSpan}>학력</span>
-        {educations.map((education) => (
-          <span key={education}>{education}</span>
-        ))}
-      </section>
-      <section className={styles.descriptionSection}>
-        <span className={styles.titleSpan}>이력</span>
-        {careers.map((career) => (
-          <span key={career}>{career}</span>
-        ))}
-      </section>
-      <ArtistActivity />
+      <div>
+        <button>으아</button>
+      </div>
+      <ArtistActivity description={artistData.description} />
+      <Link className={styles.backButton} to='/artists'>
+        목록으로 돌아가기
+      </Link>
     </div>
   );
 }
