6f33ee5d84eae1d66dcedd5ffd74da9343cc77c5
P1su <pissssssu@naver.com>
2025-06-03 01:06:11 +0900

    fix: 특별전시 캐러셀 파라미터 수정

--- client/src/pages/Main/Main.jsx
+++ client/src/pages/Main/Main.jsx
@@ -11,8 +11,8 @@ export default function Main() {
       <SwipeCarousel title='현재 진행 중인 전시' />
       <SwipeCarousel
         title='놓치지 말아야 할 특별 전시'
-        category='category'
-        value='특별전시'
+        category='sort'
+        value='latest'
       />
       <SwipeCarousel title='인기있는 전시' category='sort' value='popular' />
       <ArtistCarousel title='전시 중인 작가' />
