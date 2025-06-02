9ba22f8bb57ad5193fb46774fb85797dac1bc276
P1su <pissssssu@naver.com>
2025-06-02 15:36:39 +0900

    feat: 전시 상세 페이지 스타일 수정 및 좋아요 기능 구현

--- client/src/pages/ExhibitionDetail/ExhibitionDetail.jsx
+++ client/src/pages/ExhibitionDetail/ExhibitionDetail.jsx
@@ -1,17 +1,28 @@
 import styles from './ExhibitionDetail.module.css';
-import { useParams, Link, useNavigate } from 'react-router-dom'; // useNavigate 추가
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { instance, userInstance } from '../../apis/instance.js';
 import { FaQrcode, FaCalendar, FaHeart, FaShare } from 'react-icons/fa';
-import BtnFavorite from './components/BtnFavorite/BtnFavorite';
-import BtnShare from './components/BtnShare/BtnShare';
-import StickyMenu from './components/StikcyMenu/StickyMenu';
 
 export default function ExhibitionDetail() {
   const { exhibitionId } = useParams();
-  const navigate = useNavigate(); // 컴포넌트 내부에 추가
-  const [ExhibitionData, setExhibitionData] = useState([]);
-  const [isLike, setIsLike] = useState('false');
+  const navigate = useNavigate();
+  const [exhibitionData, setExhibitionData] = useState([]);
+  const {
+    exhibition_title: title,
+    exhibition_poster: poster,
+    exhibition_start_date: startDate,
+    exhibition_end_date: endDate,
+    exhibition_start_time: startTime,
+    exhibition_end_time: endTime,
+    exhibition_location: exhibitionLocation,
+    exhibition_price: price,
+    exhibition_organization: organization,
+    exhibition_closed_day: closedDay,
+    exhibition_description: description,
+    related_exhibitions: relatedExhibitions,
+    is_liked: isLike,
+  } = exhibitionData;
 
   const getExhibitionDetail = async () => {
     try {
@@ -27,61 +38,65 @@ export default function ExhibitionDetail() {
     getExhibitionDetail();
   }, []);
 
-  const handleLike = () => {
-    const postExhibitionLike = async () => {
-      try {
+  const handleLike = async () => {
+    try {
+      if (isLike === true) {
+        await userInstance.delete('/api/likes', {
+          data: {
+            liked_id: exhibitionId,
+            liked_type: 'exhibition',
+          },
+        });
+      } else {
         await userInstance.post('/api/likes', {
           liked_id: exhibitionId,
           liked_type: 'exhibition',
         });
-      } catch (error) {
-        console.error(error);
-        alert('좋아요 실패');
       }
-    };
 
-    const deleteExhibitionLike = async () => {
-      try {
-        await userInstance.delete('/api/likes', {
-          liked_id: exhibitionId,
-          liked_type: 'exhibition',
-        });
-      } catch (error) {
-        console.error(error);
-        alert('좋아요 실패');
-      }
-    };
-
-    if (isLike) {
-      deleteExhibitionLike();
-      setIsLike(false);
-    } else {
-      postExhibitionLike();
-      setIsLike(true);
+      await getExhibitionDetail();
+    } catch (error) {
+      console.error(error);
+      alert('좋아요 처리 실패');
     }
   };
 
-  const {
-    id,
-    exhibition_title: title,
-    exhibition_poster: poster,
-    exhibition_category: category,
-    exhibition_tag: tag,
-    exhibition_start_date: startDate,
-    exhibition_end_date: endDate,
-    exhibition_start_time: startTime,
-    exhibition_end_time: endTime,
-    exhibition_location: exhibitionLocation,
-    exhibition_price: price,
-    exhibition_organization: organization,
-    exhibition_closed_day: closed_day,
-    exhibition_description: description,
-    related_exhibitions: relatedExhibitions,
-  } = ExhibitionData;
+  const infos = [
+    {
+      key: 'date',
+      label: '기간',
+      content: `${startDate} ~ ${endDate}`,
+    },
+    {
+      key: 'time',
+      label: '시간',
+      content: `${startTime} ~ ${endTime}`,
+    },
+    {
+      key: 'place',
+      label: '장소',
+      content: organization,
+    },
+    {
+      key: 'location',
+      label: '주소',
+      content: exhibitionLocation,
+    },
+    {
+      key: 'closed',
+      label: '휴관일',
+      content: closedDay,
+    },
+    {
+      key: 'price',
+      label: '관람료',
+      content: `${price} (원)`,
+    },
+  ];
 
   const buttons = [
     {
-      label: '관심 작가',
+      label: '좋아요',
       icon: (
         <FaHeart className={`${styles.icon} ${isLike && styles.icHeart}`} />
       ),
@@ -103,28 +118,24 @@ export default function ExhibitionDetail() {
       label: '도슨트',
       icon: <FaQrcode className={styles.icon} />,
       action: () => navigate(`/scan`),
-    },    
+    },
   ];
 
-  
   return (
     <div className={styles.layout}>
-      <StickyMenu id={exhibitionId} />
+      <h1 className={styles.title}>{title}</h1>
 
-      <section className={styles.titleSection}>
-        <img
-          className={styles.exhibitionImage}
-          src={poster}
-          alt='전시회 대표 이미지'
-        />          
-        <span className={styles.titleSpan}>{title}</span>
-      </section>
+      <img
+        className={styles.exhibitionImage}
+        src={poster}
+        alt='전시회 대표 이미지'
+      />
 
       <div className={styles.buttonContainer}>
         {buttons.map(({ label, icon, action }) => (
-          <button className={styles.subButton} key={label} onClick={action}>
+          <button className={styles.buttonBox} key={label} onClick={action}>
             {icon}
-            <span>{label}</span>
+            {label}
           </button>
         ))}
       </div>
@@ -132,56 +143,24 @@ export default function ExhibitionDetail() {
       <hr className={styles.divider} />
 
       <section className={styles.infoSection}>
-        <div className={styles.infoContainer}>
-          <span>전시기간</span>
-          <span>
-            {startDate} - {endDate}
-          </span>
-        </div>
-        <div className={styles.infoContainer}>
-          <span>전시장소</span>
-          <span>
-            {organization}
-          </span>
-        </div>
-        <div className={styles.infoContainer}>
-          <span>관람시간</span>
-          <span>
-            {startTime} - {endTime}
-          </span>
-        </div>
-        <div className={styles.infoContainer}>
-          <span>휴관일</span>
-          <span>
-            {closed_day}
-          </span>
-        </div>
-        <div className={styles.infoContainer}>
-          <span>입장료</span>
-          <span>{price}원</span>
-        </div>
-        <div className={styles.infoContainer}>
-          <span>주소</span>
-          <span>{exhibitionLocation}</span>
-        </div>
+        {infos.map(({ key, label, content }) => (
+          <div className={styles.infoContainer} key={key}>
+            <span className={styles.infoSpan}>{label}</span>
+            <p className={styles.infoParagraph}>{content}</p>
+          </div>
+        ))}
       </section>
 
-      <h2> 전시 정보 </h2>
+      <h2 className={styles.subTitle}> 전시 정보 </h2>
+      <p className={styles.subParagraph}>{description}</p>
+      <span className={styles.subSpan}>
+        ※ 아뜰리에 등록된 이미지와 글의 저작권은 각 작가와 필자에게 있습니다.
+      </span>
 
-      <hr className={styles.divider} />
-
-      <div className={styles.infoContainer}>
-        <span>
-          {description}
-        </span>
-      </div>
-
-      <h2> 전시 작품 </h2>
-
-      <hr className={styles.divider} />
+      <h2 className={styles.subTitle}> 관련 전시 </h2>
 
       <div className={styles.relatedExhibitionGrid}>
-        {Array.isArray(relatedExhibitions) && relatedExhibitions.length > 0 ? (
+        {relatedExhibitions?.length > 0 ? (
           relatedExhibitions.map((exhibition) => (
             <Link
               to={`/exhibition/${exhibition.id}`}
@@ -194,11 +173,13 @@ export default function ExhibitionDetail() {
                 className={styles.relatedExhibitionImage}
               />
               <div className={styles.relatedExhibitionInfo}>
-                <h4>{exhibition.title}</h4>
-                <p className={styles.location}>
+                <h4 className={styles.relatedExhibitionTitle}>
+                  {exhibition.title}
+                </h4>
+                <p className={styles.relatedExhibitionPar}>
                   {exhibition.organization}
                 </p>
-                <p className={styles.location}>
+                <p className={styles.relatedExhibitionSubPar}>
                   {exhibition.start_date} - {exhibition.end_date}
                 </p>
               </div>
@@ -209,7 +190,9 @@ export default function ExhibitionDetail() {
         )}
       </div>
 
-
+      <Link className={styles.backButton} to='/exhibitions'>
+        목록으로 돌아가기
+      </Link>
     </div>
   );
 }
