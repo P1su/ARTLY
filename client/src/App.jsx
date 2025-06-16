95d0a4fdd494116840b989a7755ee19bb0130a63
P1su <pissssssu@naver.com>
2025-06-16 20:39:11 +0900

    test commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import router from './router/Router';
 import { RouterProvider } from 'react-router-dom';
 
 function App() {
+  //테스트 주석
   return <RouterProvider router={router} />;
 }
 export default App;
