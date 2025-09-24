d02bce463ee4257e6b64a1ad13f944ddf02f8a54
P1su <pissssssu@naver.com>
2025-09-25 01:37:49 +0900

    feat: root display 속성 추가 및 Outlet 스타일 수정으로 footer를 하단에 고정

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -19,7 +19,9 @@ export default function Layout() {
       <ScrollToTop />
       <Header />
       <NavBar />
-      <Outlet />
+      <div style={{ flex: 1 }}>
+        <Outlet />
+      </div>
       {showChatbot && <ChatbotWidget />}
 
       <Footer />
