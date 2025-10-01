6afd58d5a149c1af80ef0a50d3cdaf6ac0e6c3da
P1su <pissssssu@naver.com>
2025-10-01 20:58:28 +0900

    feat: Login과 LoginDetail 페이지 구분

---
+++ client/src/pages/Auth/LoginDetail/LoginDetail.jsx
@@ -0,0 +1,76 @@
+import styles from './LoginDetail.module.css';
+import { instance } from '../../../apis/instance.js';
+import { useNavigate, useLocation } from 'react-router-dom';
+import useInput from '../../../hooks/useInput';
+import InputContainer from '../../../components/Input/InputConatiner/InputContainer';
+import InputText from '../../../components/Input/InputText/InputText';
+import BtnPrimary from '../../../components/BtnPrimary/BtnPrimary';
+import SocialLoginSection from './components/SocialLoginSection/SocialLoginSection';
+import SupportSection from './components/SupportSection/SupportSection';
+
+export default function LoginDetail() {
+  const { data: loginDatas, handleChange } = useInput({
+    login_id: '',
+    login_pwd: '',
+  });
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathname = location.state?.from;
+
+  const postLogin = async () => {
+    try {
+      const response = await instance.post('/api/auth/login', loginDatas);
+      localStorage.setItem('ACCESS_TOKEN', response.data.jwt);
+      /*
+      window.dispatchEvent(
+        new StorageEvent('storage', {
+          key: 'ACCESS_TOKEN',
+          newValue: response.data.jwt,
+        }),
+      );
+*/
+      if (pathname === '/register') {
+        navigate('/');
+      } else {
+        navigate(-1);
+      }
+    } catch (error) {
+      console.log(error);
+      if (error.status === 401) {
+        alert('아이디 및 비밀번호를 확인해주세요');
+      } else {
+        alert('서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
+      }
+    }
+  };
+
+  return (
+    <div className={styles.layout}>
+      <h1 className={styles.loginTitle}>로그인</h1>
+      <p className={styles.subParagraph}>아뜰리 계정으로 로그인하세요</p>
+      <div className={styles.contentContainer}>
+        <form className={styles.form} action={postLogin}>
+          <InputContainer title='아이디'>
+            <InputText
+              placeholder='ID (6 ~ 20자 영문, 숫자)'
+              name='login_id'
+              onChange={handleChange}
+              value={loginDatas.login_id}
+            />
+          </InputContainer>
+          <InputContainer title='비밀번호'>
+            <InputText
+              placeholder='Password (8 ~ 16자 영문+숫자+특수문자)'
+              name='login_pwd'
+              onChange={handleChange}
+              value={loginDatas.login_pwd}
+            />
+          </InputContainer>
+          <BtnPrimary label='로그인' />
+        </form>
+        <SupportSection />
+        <SocialLoginSection />
+      </div>
+    </div>
+  );
+}
--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -9,7 +9,6 @@ import Artists from '../pages/Category/Artist/Artists/Artists';
 import ArtistDetail from '../pages/Category/Artist/ArtistDetail/ArtistDetail';
 import Galleries from '../pages/Category/Gallery/Galleries/Galleries';
 import GalleryDetail from '../pages/Category/Gallery/GalleryDetail/GalleryDetail';
-import Login from '../pages/Auth/Login/Login';
 import Register from '../pages/Auth/Register/Register';
 import Mypage from '../pages/Auth/MyPage/MyPage';
 import EditProfile from '../pages/Auth/EditProfile/EditProfile';
@@ -30,6 +29,8 @@ import Reservation from '../pages/Reservation/Reservation';
 import ConsoleMain from '../pages_console/ConsoleMain/ConsoleMain';
 import ConsoleDetail from '../pages_console/ConsoleDetail/ConsoleDetail';
 import LayoutConsole from '../layouts/LayoutConsole';
+import LoginDetail from './../pages/Auth/LoginDetail/LoginDetail';
+import Login from './../pages/Auth/Login/Login';
 
 const router = createBrowserRouter([
   {
@@ -52,6 +53,7 @@ const router = createBrowserRouter([
       { path: '/catalog/:catalogId', element: <Catalog /> },
       { path: '/nearby-galleries', element: <Nearby /> },
       { path: '/login', element: <Login /> },
+      { path: '/login-detail', element: <LoginDetail /> },
       { path: '/register', element: <Register /> },
       { path: '/find', element: <Find /> },
       { path: '/announcement', element: <Announcement /> },
