df3924548a3b47a4b4c95f88ff171c8c04e6b845
MINJU <146050842+MINJU-62@users.noreply.github.com>
2025-10-27 18:36:09 +0900

    [FIX] 뒤로가기 로직 변경

--- client/src/layouts/AuthRoute.jsx
+++ client/src/layouts/AuthRoute.jsx
@@ -4,6 +4,6 @@ export default function AuthRoute() {
   return localStorage.getItem('ACCESS_TOKEN') ? (
     <Outlet />
   ) : (
-    <Navigate to='/login' />
+    <Navigate to='/login' replace={true} />
   );
 }
