a6740eed7530368aeb6b4b51dd5ae84104c396fa
P1su <pissssssu@naver.com>
2025-11-07 00:07:45 +0900

    feat: UserProvider 통해 유저 데이터 호출 및 마이페이지 영역 데이터 호출 부분 삭제 후 Context 사용하도록 수정

--- client/src/store/UserProvider.jsx
+++ client/src/store/UserProvider.jsx
@@ -1,9 +1,25 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
+import { userInstance } from './../apis/instance';
 
 export const UserContext = createContext();
 
 export function UserProdiver({ children }) {
   const [user, setUser] = useState(null); // 사용자 정보 저장
+
+  useEffect(() => {
+    if (localStorage.getItem('ACCESS_TOKEN')) {
+      const fetchUserInfo = async () => {
+        try {
+          const res = await userInstance.get('api/users/me');
+          setUser(res.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      fetchUserInfo();
+    }
+  }, []);
+
   const [token, setToken] = useState(null);
   const login = (userData, jwt) => {
     setUser(userData);
@@ -17,7 +33,7 @@ export function UserProdiver({ children }) {
     localStorage.removeItem('ACCESS_TOKEN');
   };
 
-  const value = { user, token, login, logout };
+  const value = { user, setUser, token, login, logout };
 
   console.log(user);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
