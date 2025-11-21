af87737cbc9f9726b04657a44957aa45f7279898
P1su <pissssssu@naver.com>
2025-11-21 13:31:37 +0900

    feat: 알림 페이지에서 푸터 숨기기

--- client/src/layouts/AuthRoute.jsx
+++ client/src/layouts/AuthRoute.jsx
@@ -1,19 +1,22 @@
-import { Outlet, Navigate } from 'react-router-dom';
+import { Outlet, Navigate, useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 import { UserContext } from '../store/UserProvider.jsx';
 import Footer from './../components/Footer/Footer';
 
 export default function AuthRoute() {
   const { user } = useContext(UserContext);
+  const location = useLocation();
 
   if (!user || !user.id) {
     return <Navigate to='/login' replace />;
   }
 
+  const showFooter = location.pathname !== '/notifications';
+
   return (
     <>
       <Outlet />
-      <Footer />
+      {showFooter && <Footer />}
     </>
   );
 }
