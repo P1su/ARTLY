1e90a378afeb009b58c6af7193fa7bce305e87b0
홍유진 <dbwlsyyy@naver.com>
2025-11-26 22:43:02 +0900

    design: attendance 모달 디자인 수정

--- client/src/pages/Auth/MyPage/components/AttendanceModal/AttendanceModal.jsx
+++ client/src/pages/Auth/MyPage/components/AttendanceModal/AttendanceModal.jsx
@@ -31,6 +31,8 @@ const AttendanceModal = ({
         </div>
 
         <div className={styles.confirmText}>관람 확인 완료</div>
+        <div className={styles.visitDate}>이용해주셔서 감사합니다.</div>
+
         <div className={styles.visitDate}>방문일시 : {visitDate}</div>
 
         <button className={styles.infoButton} onClick={onViewExhibition}>
