33c760c292390c1b783c2156a435e59dd54087da
홍유진 <dbwlsyyy@naver.com>
2025-11-28 16:00:31 +0900

    fix: 작품 설명 미리보기 수정

--- client/src/pages/Category/Artwork/ArtworkList/components/ArtworkCard/ArtworkCard.jsx
+++ client/src/pages/Category/Artwork/ArtworkList/components/ArtworkCard/ArtworkCard.jsx
@@ -8,11 +8,15 @@ export default function ArtworkList({ artworkItems }) {
     art_title: title,
     art_description: description,
   } = artworkItems;
+  const cleanDescription = description
+    ? description.replace(/(<([^>]+)>)/gi, '')
+    : '정보 없음';
+
   return (
     <Link className={styles.layout} to={`/artworks/${id}`}>
       <img className={styles.artworkImage} src={image} alt='대표 이미지' />
       <h3 className={styles.artworkTitle}>{title}</h3>
-      <p className={styles.subParagraph}>{description}</p>
+      <p className={styles.subParagraph}>{cleanDescription}</p>
     </Link>
   );
 }
