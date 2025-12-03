6fbd05f246d65ec9f8b8dd80bc219755b45308af
dbwlsyyy <148929571+dbwlsyyy@users.noreply.github.com>
2025-12-03 16:06:05 +0900

    [REFACTOR] 메인화면 재디자인 및 수정 (#172)
    * design: 챗봇 디자인 수정
    
    * design: mobile main 페이지 디자인 수정
    
    * refactor: main 모바일, pc 재디자인 및 플로우 구분
    
    * fix: 도슨트 qr 경로 수정
    
    * refactor: ux상 불필요한 로그아웃 모달 제거
    
    * design: 헤더, 사이드바, 챗봇 간단 수정

--- client/src/assets/svg/IcDocent.jsx
+++ client/src/assets/svg/IcDocent.jsx
@@ -1,8 +1,8 @@
 const IcDocent = () => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
-    width='40'
-    height='31'
+    width='43'
+    height='44'
     viewBox='0 0 40 31'
     fill='none'
   >
